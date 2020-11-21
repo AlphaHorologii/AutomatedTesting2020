e015cfeb719acf968ce367b5f18ea29da2c31483
AlphaHorologii <46262090+AlphaHorologii@users.noreply.github.com>
2020-11-21 17:29:06 +0800

    add .gitignore

--- Project/src/main/java/MyTestController.java
+++ Project/src/main/java/MyTestController.java
@@ -1,9 +1,7 @@
 import com.ibm.wala.classLoader.IClass;
+import com.ibm.wala.classLoader.IMethod;
 import com.ibm.wala.classLoader.Language;
-import com.ibm.wala.ipa.callgraph.AnalysisCacheImpl;
-import com.ibm.wala.ipa.callgraph.AnalysisOptions;
-import com.ibm.wala.ipa.callgraph.AnalysisScope;
-import com.ibm.wala.ipa.callgraph.CGNode;
+import com.ibm.wala.ipa.callgraph.*;
 import com.ibm.wala.ipa.callgraph.cha.CHACallGraph;
 import com.ibm.wala.ipa.callgraph.impl.AllApplicationEntrypoints;
 import com.ibm.wala.ipa.callgraph.impl.Util;
@@ -65,7 +63,7 @@ public class MyTestController {
                 while(nodeIterator.hasNext()){
                     CGNode childNode = nodeIterator.next();
                     for (String classname: iClasses) {
-                        if(childNode.toString().contains(classname.concat(",")) && !parentNode.toString().contains(classname.concat(","))){
+                        if(childNode.toString().contains(classname.concat(","))){
                             String parent = parentNode.toString().split(",")[1].substring(1);
                             String child = childNode.toString().split(",")[1].substring(1);
                             if(dependencies.get(parent) != null){
@@ -91,21 +89,21 @@ public class MyTestController {
             fw.flush();
             fw.close();
 
-            BufferedReader iStream = new BufferedReader(new FileReader(new File(args[2])));
+            BufferedReader iReader = new BufferedReader(new FileReader(new File(args[2])));
             FileWriter selection = new FileWriter("selection-class.txt");
             String temp = "";
             ArrayList<String> selectedClass = new ArrayList<String>();
-            while ((temp = iStream.readLine()) != null){
+            HashMap<String, Boolean> visited = new HashMap<String, Boolean>();
+            for(String key: dependencies.keySet()){
+                visited.put(key, false);
+            }
+            while ((temp = iReader.readLine()) != null){
                 String classname = temp.split(" ")[0];
-                for(String key: dependencies.keySet()){
-                    if(dependencies.get(key).contains(classname) && !selectedClass.contains(key) && key.contains("Test")){
-                        selectedClass.add(key);
-                    }
-                }
+                selectClass(classname, dependencies, selectedClass, visited);
             }
             for(CGNode cgNode: appNodes){
                 for(String classname: selectedClass){
-                    if(cgNode.toString().contains(classname + ",") && !cgNode.toString().contains("<init>")){
+                    if(cgNode.toString().contains(classname + ",") && !cgNode.toString().contains("<init>") && !cgNode.toString().contains("initialize()")){
                         selection.write(cgNode.getMethod().toString().split(" ")[2].replace(',', ' ') +
                                 cgNode.getMethod().toString().split(" ")[2].replace(',', '.').replaceAll("/", ".").substring(1) +
                                 cgNode.getMethod().toString().split(" ")[3] +
@@ -120,8 +118,105 @@ public class MyTestController {
             AnalysisOptions options = new AnalysisOptions(scope, entryPoints);
             SSAPropagationCallGraphBuilder builder = Util.makeZeroCFABuilder(Language.JAVA, options, new AnalysisCacheImpl(), cha, scope);
 
+            CallGraph cg = builder.makeCallGraph(options);
 
+            ArrayList<CGNode> appNodes = new ArrayList<CGNode>();
+            for (CGNode cgNode: cg) {
+                if(cgNode.getMethod().toString().startsWith("< Application"))
+                    appNodes.add(cgNode);
+            }
 
+            HashMap<String, ArrayList<String>> dependencies = new HashMap<String, ArrayList<String>>();
+            for (CGNode parentNode: appNodes) {
+                Iterator<CGNode> nodeIterator = cg.getSuccNodes(parentNode);
+                while(nodeIterator.hasNext()){
+                    CGNode childNode = nodeIterator.next();
+                    if(childNode.getMethod().toString().startsWith("< Application")){
+                        String parentMethod = parentNode.getMethod().toString().split(" ")[2].replace(',', '.').replaceAll("/", ".").substring(1) +
+                                parentNode.getMethod().toString().split(" ")[3];
+                        String childMethod = childNode.getMethod().toString().split(" ")[2].replace(',', '.').replaceAll("/", ".").substring(1) +
+                                childNode.getMethod().toString().split(" ")[3];
+                        if(dependencies.get(parentMethod) != null){
+                            if(!dependencies.get(parentMethod).contains(childMethod)){
+                                dependencies.get(parentMethod).add(childMethod);
+                            }
+                        }
+                        else {
+                            dependencies.put(parentMethod, new ArrayList<String>());
+                            dependencies.get(parentMethod).add(childMethod);
+                        }
+                    }
+                }
+            }
+            for (String key: dependencies.keySet()) {
+                for (String item: dependencies.get(key)) {
+                    fw.write("\"" + key + "\"->\"" + item + "\"\n");
+                }
+                fw.flush();
+            }
+            fw.write("}\n");
+            fw.flush();
+            fw.close();
+
+            BufferedReader iReader = new BufferedReader(new FileReader(new File(args[2])));
+            FileWriter selection = new FileWriter("selection-method.txt");
+            String temp = "";
+            ArrayList<String> selectedMethod = new ArrayList<String>();
+            HashMap<String, Boolean> visited = new HashMap<String, Boolean>();
+            for(String key: dependencies.keySet()){
+                visited.put(key, false);
+            }
+            while ((temp = iReader.readLine()) != null){
+                String methodName = temp.split(" ")[1];
+                selectMethod(methodName, dependencies, selectedMethod, visited);
+            }
+
+            for(String methodName: selectedMethod){
+                if(!methodName.contains("<init>")) {
+                    StringBuilder sBuilder = new StringBuilder("L");
+                    for (int i = 0; i < methodName.split("\\.").length - 1; i++) {
+                        sBuilder.append(methodName.split("\\.")[i]);
+                        if (i != methodName.split("\\.").length - 2) {
+                            sBuilder.append('/');
+                        }
+                    }
+                    selection.write(sBuilder.toString() + " " + methodName + "\n");
+                }
+            }
+            selection.flush();
+            selection.close();
+        }
+    }
+
+    static void selectClass(String classname, HashMap<String, ArrayList<String>> dependencies, ArrayList<String> selectedClass, HashMap<String, Boolean> visited) {
+        for(String key: dependencies.keySet()){
+            if(dependencies.get(key).contains(classname) && !selectedClass.contains(key)){
+                if(key.contains("Test")) {
+                    selectedClass.add(key);
+                }
+                else {
+                    if(!visited.get(key)) {
+                        visited.put(key, true);
+                        selectClass(key, dependencies, selectedClass, visited);
+                    }
+                }
+            }
+        }
+    }
+
+    static void selectMethod(String methodName, HashMap<String, ArrayList<String>> dependencies, ArrayList<String> selectedMethod, HashMap<String, Boolean> visited) {
+        for(String key: dependencies.keySet()){
+            if(dependencies.get(key).contains(methodName) && !selectedMethod.contains(key)){
+                if(key.contains("Test")) {
+                    selectedMethod.add(key);
+                }
+                else {
+                    if(!visited.get(key)) {
+                        visited.put(key, true);
+                        selectMethod(key, dependencies, selectedMethod, visited);
+                    }
+                }
+            }
         }
     }
 
